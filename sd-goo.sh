b404c18298421f526958aac26a584ea23538a997
Kamal Deep Bhati <th.kamalsinghrajput@gmail.com>
2021-05-07 16:35:18 +0530

    Update sd-goo.sh

--- sd-goo.sh
+++ sd-goo.sh
@@ -3,11 +3,12 @@
 #sudo /etc/init.d/tor restart
 #torify
 
+rm -rf gsd-tmp1.txt &> /dev/null
 tar=$1
 sfe=$(echo "$tar" | cut -d "." -f 1)
 echo -n "+-www" > gsd-tmp.txt
 
-runtime="1 minute"
+runtime="40 second"
 endtime=$(date -ud "$runtime" +%s)
 
 while [[ $(date -u +%s) -le $endtime ]]
@@ -19,9 +20,11 @@ g=$(curl "https://www.google.com/search?q=site:$tar$gsd" -A 'Mozilla/5.0 (Window
 if echo "$g" | grep -i ".$tar" &> /dev/null;
   then
     echo "$g" | sed 's/https:\/\///g' | sed 's/http:\/\///g'
-    echo "$g" | grep -v "https://$tar\|http://$tar" | cut -d '/' -f 3 | cut -d "." -f 1 | sed 's/\<$sfe\>//g' | sed 's/^/+-/' | sort -u | tr -d '\n' | tr '+-' '\n' | sort -u | sed 's/^/+-/' | tr -d '\n' >> gsd-tmp.txt
+    echo "$g" | grep -v "https://$tar\|http://$tar" | cut -d '/' -f 3 | cut -d "." -f 1 | sed 's/\<$sfe\>//g' | sed 's/^/+-/' | sort -u | tr -d '\n' >> gsd-tmp1.txt
 else
         exit 1
 fi
 done < gsd-tmp.txt
+
+cat gsd-tmp1.txt | tr '+-' '\n' | sort -u | sed 's/^/+-/' | tr -d '\n' > gsd-tmp.txt
 done
