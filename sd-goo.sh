468e1fc207928cb7e8ff0a69fb4bd68208fe05cd
Kamaldeep Bhati <th.kamalsinghrajput@gmail.com>
2025-03-18 22:17:49 -0400

    Update sd-goo.sh

--- sd-goo.sh
+++ sd-goo.sh
@@ -15,7 +15,8 @@ while [[ $(date -u +%s) -le $endtime ]]
 do
 
 for gsd in $(cat gsd-tmp.txt); do
-g=$(curl "https://www.google.com/search?q=site:$tar$gsd" -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' -s | grep -Eoi '<a [^>]+>' |  grep -Eo 'href="[^\"]+"' |  grep -Eo '(http|https)://[^/"]+' | grep -i "$tar" | sort -u)
+#g=$(curl "https://www.google.com/search?q=site:$tar$gsd" -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' -s | grep -Eoi '<a [^>]+>' |  grep -Eo 'href="[^\"]+"' |  grep -Eo '(http|https)://[^/"]+' | grep -i "$tar" | sort -u)
+g=$(curl "https://www.google.com/search?q=site:$tar$gsd" -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' -H 'Cookie: __Secure-3PSID=g.a000uwikMPseUBcqtz0kR67o-ILy-mh7O_w3p7OJ_OK2TQZo5I4fipPDAM0KjIKHobsgW_1a8gACgYKATISARESFQHGX2MigU0PDc9PvQPzi7J8yW1aKxoVAUF8yKo99dbhgMbRutFM3QSgcGyf0076; __Secure-3PSIDTS=sidts-CjEB7pHptTwIfzoQL4PUz2Bdnu5bC2B6Af8ToAVBWG2xx9hFP7olP1OmElFLYpxsw__kEAA' -s | grep -Eoi '<a [^>]+>' |  grep -Eo 'href="[^\"]+"' |  grep -Eo '(http|https)://[^/"]+' | grep -i "$tar" | sort -u)
 
 if echo "$g" | grep -i ".$tar" &> /dev/null;
   then
